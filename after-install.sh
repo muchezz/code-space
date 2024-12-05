322461a2c847eab3aec2a46cd4fdf702e9db9049
Mucheru Maina <mucheru@outlook.com>
2024-12-05 18:22:51 +0300

    Update after-install.sh

--- after-install.sh
+++ after-install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Install Azure CLI
-curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
-AZ_REPO=$(lsb_release -cs) && echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
-sudo apt update && sudo apt install -y azure-cli make gcc
-
 # Install Go
 GO_LANG_VERSION=1.19
 curl -L -o go.tar.gz "https://go.dev/dl/go${GO_LANG_VERSION}.linux-amd64.tar.gz" && \
